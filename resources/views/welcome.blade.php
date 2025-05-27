2c6417b58cff2f1c7ea34c4c77afb8dac6dccc15
Alvaro Navarro <quinchonavarro@gmail.com>
2025-05-26 21:42:01 -0400

    Initial commit, add AdminLTE

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+{{-- Customize layout sections --}}
+
+@section('subtitle', 'Home')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+{{-- Content body: main page content --}}
+
+@section('content_body')
+    <p>Welcome to this admin panel.</p>
+@stop
+
+
